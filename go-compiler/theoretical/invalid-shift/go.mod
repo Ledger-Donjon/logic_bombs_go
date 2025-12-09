24a15ac0ad7be60ef191d5e9f7ff7c44bf3e347a
kajaaz <karolinagorna@hotmail.fr>
2025-12-09 16:21:57 +0100

    add go-compiler category

---
+++ go-compiler/theoretical/invalid-shift/go.mod
@@ -0,0 +1,3 @@
+module invalid-shift
+
+go 1.25.1
---
+++ go-compiler/theoretical/invalid-shift/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"runtime"
+)
+
+func shift(s string) {
+	var arr [64]byte
+	idx := int(s[0])        // '@' == 64 -> arr[64] -> panic
+	_ = arr[idx]            // triggers bounds check and panic when idx >= 64
+	fmt.Println("ok")
+}
+
+func main() {
+	runtime.GOMAXPROCS(1)
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: ./invalid-shift [byte]")
+		return
+	}
+	s := os.Args[1]
+	if len(s) == 0 { return }
+
+	shift(s)
+}
+
